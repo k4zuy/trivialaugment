0ef3da88be1a8c122ed2c188202d3eb7ed215c43
k4zuy <102243006+k4zuy@users.noreply.github.com>
2022-05-15 08:34:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,21 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # get the dependencies and installs
-with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
-    all_reqs = f.read().split("\n")
+#with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+ #   all_reqs = f.read().split("\n")
 
-install_requires = [x.strip() for x in all_reqs if "git+" not in x]
-dependency_links = [
-    x.strip().replace("git+", "") for x in all_reqs if x.startswith("git+")
+#install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+#dependency_links = [
+#    x.strip().replace("git+", "") for x in all_reqs if x.startswith("git+")
 ]
+
+REQUIRES = []
+with open('requirements.txt') as f:
+    REQUIRES = f.readlines()
 
 setup(
   name='trivialaugment',    # This is the name of your PyPI-package.
   version='1.0',            # Update the version number for new releases
-  install_requires=install_requires,
-  dependency_links=dependency_links
+  install_requires=REQUIRES
+  #dependency_links=dependency_links
 )
